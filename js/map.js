8c9cf7813f2359672deccca8fc232e3b67dddf46
Dave <86604312+dfaso@users.noreply.github.com>
2026-09-17 14:23:47 +0930

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,1 +1,11 @@
-// Main map JavaScript
+const map = new maplibregl.Map({
+  container: "map",
+  style: "https://tiles.openfreemap.org/styles/liberty",
+  center: [138.6, -34.9],
+  zoom: 6
+});
+
+map.addControl(
+  new maplibregl.NavigationControl(),
+  "top-left"
+);
