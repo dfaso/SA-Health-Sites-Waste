dbc5452f363c6e7929ed2cab567905c68b43e601
Dave <86604312+dfaso@users.noreply.github.com>
2026-09-17 14:26:49 +0930

    Update map.js

--- js/map.js
+++ js/map.js
@@ -9,3 +9,27 @@ map.addControl(
   new maplibregl.NavigationControl(),
   "top-left"
 );
+
+map.on("load", () => {
+
+  // Waste site GeoJSON
+  map.addSource("waste-sites", {
+    type: "geojson",
+    data: "./data/waste_sites.geojson"
+  });
+
+  // Waste site points
+  map.addLayer({
+    id: "waste-sites",
+    type: "circle",
+    source: "waste-sites",
+
+    paint: {
+      "circle-radius": 5,
+      "circle-color": "#3388ff",
+      "circle-stroke-color": "#ffffff",
+      "circle-stroke-width": 1
+    }
+  });
+
+});
