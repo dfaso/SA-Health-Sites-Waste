a0843e71a74b5572f5ee6eb06563248a9a479209
Dave <86604312+dfaso@users.noreply.github.com>
2026-09-18 08:10:38 +0930

    Update map.js

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@ const map = new maplibregl.Map({
   container: "map",
   style: "https://tiles.openfreemap.org/styles/liberty",
   center: [138.6, -34.9],
-  zoom: 4
+  zoom: 5
 });
 
 map.addControl(
